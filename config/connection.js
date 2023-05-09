b0197f48a8c8e17806949282cdd8ceb23ba4a8ad
cjsolis <cjelizondo12@gmail.com>
2023-05-09 13:38:31 -0500

    added sequalize config connection

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = process.env.JAWSDB_URL 
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.Db_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        dialectOptions: {
+            decimalNumbers: true,
+        },
+    });
+
+module.exports = sequelize;    
