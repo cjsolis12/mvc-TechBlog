ab3951235be81b7e927d08ba0dce472bd87e0fb2
cjsolis <cjelizondo12@gmail.com>
2023-05-14 15:13:59 -0500

    fixed toLocaleDateString function for helper

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ })
+const helpers = require('./utils/helpers')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sequelize = require('./config/connection');
 //add require routes
 const routes = require('./controllers')
-//add require utils helpers
 
 
+const hbs = exphbs.create({ helpers })
 const app = express();
 const PORT = process.env.PORT || 3001
 
