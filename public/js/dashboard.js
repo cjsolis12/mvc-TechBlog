50292b5127c72de5d25f4314b336bf388542ee0c
cjsolis <cjelizondo12@gmail.com>
2023-05-22 15:43:37 -0500

    changed id for new post form and created front end and backend route

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,31 +2,25 @@ const createNewBlog = async (event) => {
     event.preventDefault();
 
     const title = document.querySelector('#blogTitle').value.trim();
-    const content = document.querySelector('#blogContent').value.trim();
+    const content = document.querySelector('#blogText').value.trim();
 
     console.log(title, content)
+
+    if(title && content){
+        const response = await fetch (`api/blogposts/`, {
+            method: 'POST',
+            body: JSON.stringify({title, content }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if(response.ok){
+            document.location.replace('/dashboard');
+        } else {
+            alert( `Failed to create new post`)
+        }
+    }
 }
 
 document.querySelector('#newPostBtn').addEventListener('click', createNewBlog)
-
-//   // Get the modal element
-//   const modal = document.getElementById('exampleModal');
-
-//   // Get the button that triggers the modal
-//   const button = document.querySelector('[data-bs-target="#exampleModal"]');
-
-//   // Add a click event listener to the button
-//   button.addEventListener('click', function () {
-//     // Show the modal by adding the 'show' class
-//     modal.classList.add('show');
-//     modal.style.display = 'block';
-//     modal.removeAttribute('aria-hidden');
-//   });
-
-//   // Hide the modal when the close button is clicked
-//   const closeButton = modal.querySelector('.btn-close');
-//   closeButton.addEventListener('click', function () {
-//     modal.classList.remove('show');
-//     modal.style.display = 'none';
-//     modal.setAttribute('aria-hidden', 'true');
-//   });
