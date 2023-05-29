c9da8842f464b55688ff498b5ba5c07a56c6db74
cjsolis <cjelizondo12@gmail.com>
2023-05-29 09:55:23 -0500

    added styling to dashboard blogposts

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,34 +1,31 @@
-const router = require('express').Router();
-const { BlogPost, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
-
+const router = require("express").Router();
+const { BlogPost, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
 //GET all user's blogs for their dashboard
-router.get('/', withAuth, async (req, res) => {
-    try{
+router.get("/", withAuth, async (req, res) => {
+    try {
         //Retrieve the current logged-in user's ID from the session
         const userId = req.session.user_id;
-        console.log(userId)
+        console.log(userId);
         //Find the logged in user based on session ID
         const userData = await User.findByPk(userId, {
-            attributes:{ exclude: ['password']},
-            include: [{ model: BlogPost }]
+            attributes: { exclude: ["password"] },
+            include: [{ model: BlogPost }],
         });
-        const user = userData.get({ plain: true});
-        console.log(user)
-        
-       
+        const user = userData.get({ plain: true });
+        console.log(user);
 
         //pass data and session flag into a template
-        res.render('dashboard', {
+        res.render("dashboard", {
             ...user,
             blogPosts: user.BlogPosts,
             logged_in: true,
         });
     } catch (err) {
-        console.log(err)
-        res.status(500).json(err)
+        console.log(err);
+        res.status(500).json(err);
     }
-    });
+});
 
 module.exports = router;
