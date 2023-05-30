34c0177acb24b053cf07cb6295a0fdfe9611ee31
cjsolis <cjelizondo12@gmail.com>
2023-05-30 13:55:08 -0500

    created delete route in dashboard.js and blogpostroutes to delete blogs from the dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -21,4 +21,20 @@ const createNewBlog = async (event) => {
     }
 }
 
+const deleteBlog = async (event) => {
+    const blogPostId = event.target.dataset.id;
+    const response = await fetch (`api/blogs/${blogPostId}`, {
+        method: 'DELETE',
+    });
+    if (response.ok){
+        location.reload();
+    } else {
+        elert('Failed to delete blog post')
+    }
+}
+
 document.querySelector('#newPostBtn').addEventListener('click', createNewBlog)
+
+document.querySelectorAll('.fa-trash-can').forEach((button) => { 
+    button.addEventListener('click', deleteBlog); 
+});
