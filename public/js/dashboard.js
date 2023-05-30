b88c4c1c9f48aed5540e530146ec35b169162be2
cjsolis <cjelizondo12@gmail.com>
2023-05-30 14:02:58 -0500

    changed route to go back to dashboard after creating blog

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -14,7 +14,7 @@ const createNewBlog = async (event) => {
         });
 
         if(response.ok){
-            document.location.replace('/');
+            document.location.replace('/dashboard');
         } else {
             alert( `Failed to create new post`)
         }
