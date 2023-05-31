a3f3155a315a1a6015d592b6db9a549811881be3
cjsolis <cjelizondo12@gmail.com>
2023-05-30 21:54:37 -0500

    changed arrow function in get route to get blog data mapped

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,42 +4,42 @@ const { BlogPost, User, Comment } = require('../models')
 
 //GET all existing blogposts for homepage
 router.get('/', async (req, res) => {
-try{
-const dbBlogPostData = await BlogPost.findAll({
-    include: [
-        {
-            model: User,
-            attributes:['username'],
-        },
-        {
-            model: Comment,
-            attributes: ['text', 'user_id', 'createdAt'],
-            include: {
-                model: User,
-                attributes:['username'],
-            },
-        }
-    ]
-});
+    try {
+        const dbBlogPostData = await BlogPost.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['text', 'user_id', 'createdAt'],
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                }
+            ]
+        });
 
- const blogs = dbBlogPostData.map((blogpost) => {
- blogpost.get({ plain: true })
- 
-});
- res.render('homepage', {
-    blogs,
-    logged_in: req.session.logged_in,
+        const blogs = dbBlogPostData.map((blogpost) => 
+            blogpost.get({ plain: true })
+        )
+        console.log(blogs)
+        res.render('homepage', {
+            blogs,
+            logged_in: req.session.logged_in,
 
- });
-} catch(err){
-    res.status(500).json(err)
-}
+        });
+    } catch (err) {
+        res.status(500).json(err)
+    }
 });
 
 
 //Login Route
 router.get('/login', (req, res) => {
-    if(req.session.logged_in){
+    if (req.session.logged_in) {
         res.redirect('/');
         return;
     }
